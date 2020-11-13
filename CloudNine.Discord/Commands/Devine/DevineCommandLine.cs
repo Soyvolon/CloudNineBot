00c04e1720a3b73bf192a5f5cffbc9754a4abeb2
Andrew Bounds <andybounds@gmail.com>
2020-11-13 00:20:08 -0500

    Added devine command line file.

---
+++ CloudNine.Discord/Commands/Devine/DevineCommandLine.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace CloudNine.Discord.Commands.Devine
+{
+    public class DevineCommandLine : CommandModule
+    {
+        [Command("devine")]
+        [Description("Devine centric command line.")]
+        [Hidden]
+        public async Task DevineCommandLineAsync(CommandContext ctx,
+            [Description("Command Line Arguments")]
+            params string[] args)
+        {
+
+        }
+
+        private async Task DevineCommandLineHelpAsync(CommandContext ctx)
+        {
+
+        }
+    }
+}
