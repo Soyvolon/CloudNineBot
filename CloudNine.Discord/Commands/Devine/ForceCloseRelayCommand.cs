c259226281c9f618526636ef96df7389981d29d1
Andrew Bounds <andybounds@gmail.com>
2020-11-13 17:53:43 -0500

    Added force close relay command.

---
+++ CloudNine.Discord/Commands/Devine/ForceCloseRelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+using CloudNine.Discord.Services;
+using CloudNine.Discord.Utilities;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace CloudNine.Discord.Commands.Devine
+{
+    public class ForceCloseRelayCommand : BaseCommandModule
+    {
+        private readonly QuoteRelayService _relay;
+
+        public ForceCloseRelayCommand(QuoteRelayService relay)
+        {
+            this._relay = relay;
+        }
+
+        [Command("forcecloserelay")]
+        [Description("Forces the active relay to close for your accoun.")]
+        [Aliases("fcrelay")]
+        [Hidden]
+        public async Task ForceCloseRelayCommandAsync(CommandContext ctx)
+        {
+            if(await _relay.TryCloseRelayAsync(ctx.User))
+            {
+                await ctx.RespondAsync("Relay force closed succesffuly.");
+            }
+            else
+            {
+                await CommandResponder.RespondCommandNotFound(ctx.Channel, ctx.Prefix);
+            }
+        }
+    }
+}
