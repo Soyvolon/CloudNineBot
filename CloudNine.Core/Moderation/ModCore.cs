92dfae3ca2d6fc905344537d931ab78f78c0189b
Andrew Bounds <andybounds@gmail.com>
2020-12-07 19:32:29 -0500

    Added Moderation core features

---
+++ CloudNine.Core/Moderation/ModCore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudNine.Core.Moderation
+{
+    public class ModCore
+    {
+        [Key]
+        public ulong GuildId { get; set; }
+
+        public ConcurrentDictionary<ulong, ConcurrentStack<Warn>> Warns { get; set; }
+
+        public ModCore() : this(0, new()) { }
+
+        public ModCore(ulong guildId) : this(guildId, new()) { }
+
+        public ModCore(ulong guildId, ConcurrentDictionary<ulong, ConcurrentStack<Warn>> warns)
+        {
+            GuildId = guildId;
+            Warns = warns;
+        }
+    }
+}
---
+++ CloudNine.Core/Moderation/Warn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace CloudNine.Core.Moderation
+{
+    public class Warn
+    {
+        [JsonProperty("user_id")]
+        public ulong UserId { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("edits")]
+        public ConcurrentStack<string> Edits { get; set; }
+        [JsonProperty("reverts")]
+        public ConcurrentStack<string> Reverts { get; set; }
+
+        public Warn() : this(0, "", new(), new()) { }
+
+        public Warn(ulong user_id) : this(user_id, "", new(), new()) { }
+
+        public Warn(ulong user_id, string message) : this(user_id, message, new(), new()) { }
+
+        [JsonConstructor]
+        public Warn(ulong user_id, string message, 
+            ConcurrentStack<string> edits, ConcurrentStack<string> reverts)
+        {
+            UserId = user_id;
+            Message = message;
+            Edits = edits;
+            Reverts = reverts;
+        }
+    }
+}
