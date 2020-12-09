35bdc98c51c7d22c007eb5910146e4e906cb434c
Andrew Bounds <andybounds@gmail.com>
2020-12-08 23:59:37 -0500

    Started work on the dashboard layout elements

--- CloudNine.Web/Startup.cs
+++ CloudNine.Web/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using CloudNine.Web.User;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +30,7 @@ namespace CloudNine.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped<LoginService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ CloudNine.Web/User/LoginService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+
+using Microsoft.Extensions.Logging;
+
+namespace CloudNine.Web.User
+{
+    public class LoginService
+    {
+        public bool LoggedIn { get; set; }
+        public DiscordGuild? ActiveGuild { get; private set; }
+
+        private string? _state;
+        private readonly ILogger _logger;
+
+        public LoginService(ILogger<LoginService> logger)
+        {
+            _logger = logger;
+            _state = null;
+
+            LoggedIn = false;
+            ActiveGuild = null;
+        }
+
+        public void StartLoginAttempt(string state)
+            => _state = state;
+
+        public bool VerifyState(string state)
+            => _state?.Equals(state) ?? false;
+    }
+}
